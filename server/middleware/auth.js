77bcf2469d1b5177e914983e4564757a0ea61c18
blu3@TOp <sarojghising13@gmail.com>
2024-04-16 20:28:20 +0545

    authroized roles

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -24,7 +24,7 @@ export const authorizeRoles = (...roles) => {
 
     return (req,res,next) => {
         if(!roles.includes(req.user.role)) {
-            new ErrorHandler(`Role ${req.user.role} is not allowed to access this resource.`);
+           return next(new ErrorHandler(`Role ${req.user.role} is not allowed to access this resource.`));
         }
         next();
     };
