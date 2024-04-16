97cf22a6c81c0d80ba82fc4d483e9af152f5713c
blu3@TOp <sarojghising13@gmail.com>
2024-04-16 17:49:31 +0545

    show totalProducts in response

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -9,17 +9,19 @@ import ErrorHandler from '../utils/errorhandler.js';
 const getAllProducts = catchAsyncErrors(async (req, res) => {
 
     const resultPerPage = 8;
+    const totalProducts = await Product.countDocuments();
 
     const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
     .filter()
     .pagination(resultPerPage);
-    
+
     const products = await apiFeature.query;
 
     res.status(200).json({
         success: true,
-        products
+        products,
+        totalProducts
     });
 });
 
